f9c0aecea0f6c096b72e79c7d183666acf45e554
Swati Kumari <ksswati97@gmail.com>
2023-08-03 15:03:14 +0530

    commit

--- pages/send_email.py
+++ pages/send_email.py
@@ -6,14 +6,17 @@ from email import encoders
 import streamlit as st
 
 # Streamlit file uploader
-filename = st.file_uploader("Enter your file: ")
-receiver_email = st.text_input("Enter receiver email")
+filename = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
+receiver_email = st.text_input("Enter receiver email-")
+cc_mails = st.text_input("Enter cc email-")
+bcc_mails = st.text_input("Enter bcc email-")
+smtp_password = st.text_input("Enter your password",type='password')
+
 
 if st.button("Send Email"):
 # Assuming you have an SMTP server to send emails
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    smtp_password = "Bangalore@123"
     sender_email = "swatikumari@mba.christuniversity.in"
     body = "Hii,\n PFA report for your references. \n\n Thank you for your time and consideration.\n\n Regards,\n Swati Kumari"
     
@@ -23,7 +26,9 @@ if st.button("Send Email"):
        msg = MIMEMultipart()
        msg["From"] = sender_email
        msg["To"] = receiver_email
-       msg["Subject"] = "Attached File"
+       msg['Cc'] = cc_mails if cc_mails else ''
+       msg['Bcc'] = bcc_mails if bcc_mails else ''
+       msg["Subject"] = "Test-Email"
        
        # Attach the body of the email
        msg.attach(MIMEText(body, 'plain'))
@@ -39,6 +44,12 @@ if st.button("Send Email"):
             f"attachment; filename= {filename.name}",
         )
           msg.attach(part)
+          
+    recipients = [receiver_email]
+    if cc_mails:
+            recipients += cc_mails.split(",")
+    if bcc_mails:
+            recipients += bcc_mails.split(",")
 
     # Connect to the SMTP server and send the email
     try:
